6194b87bc39365b7f35d18e9caa0940481adccab
TuanND61 <TuanND61@fpt.com>
2025-04-09 12:43:30 +0700

    update url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/wedding-website',
+  assetPrefix: '/wedding-website',
   eslint: {
     ignoreDuringBuilds: true,
   },
